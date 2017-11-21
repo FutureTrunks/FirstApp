0e2420aba5aa72d00646e0875d45e996ae6655f9
Kamil Kuchař <kamill.kuchar@gmail.com>
2017-11-21 10:14:44 +0100

    haah

--- App1/App1/App1/MainPage.xaml.cs
+++ App1/App1/App1/MainPage.xaml.cs
@@ -15,7 +15,12 @@ namespace App1
         }
         public void Palindrome(string input)
         {
-
+            int a = 69;
         }
+        void Nevim_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var oldText = e.OldTextValue;
+            var newText = e.NewTextValue;
+}
     }
 }
