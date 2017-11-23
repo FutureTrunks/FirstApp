9f54e2e84db945cf810512756cb306c497a8c1bb
Kamil Kuchař <kamill.kuchar@gmail.com>
2017-11-23 12:22:18 +0100

    asddd

---
+++ kontakt/kontakt/kontakt/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ImageCircle.Forms.Plugin.Abstractions;
+
+namespace kontakt
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            /*CircleImage homer = new CircleImage
+            {
+                BorderColor = Color.White,
+                BorderThickness = 3,
+                HeightRequest = 150,
+                WidthRequest = 150,
+                Aspect = Aspect.AspectFill,
+                HorizontalOptions = LayoutOptions.Center,
+                Source = UriImageSource.FromUri(new Uri(""))
+            };*/
+            //main.Children.Add(homer);
+        }
+    }
+}
